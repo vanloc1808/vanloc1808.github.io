d8c764478194db7657ebf72c961d04ce471099fb
root <=>
2023-08-02 20:00:27 +0700

    Edit files

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -36,13 +36,6 @@ export default function Greeting(props) {
               </div>
             </div>
           </div>
-          <div className="greeting-image-div">
-            <img
-              alt="saad sitting on table"
-              src={require("../../assests/images/feelingProud.svg")}
-            ></img>
-            {/* <FeelingProud theme={theme} /> */}
-          </div>
         </div>
       </div>
     </Fade>
