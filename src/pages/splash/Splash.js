cf1a3c3e5257f7736e9cd6cfcd245e2f7781324c
Van-Loc Nguyen <vanloc1808@gmail.com>
2023-12-21 20:58:54 +0700

    Update usage of Redirect

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Splash.css";
-import { Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import LoaderLogo from "../../components/Loader/LoaderLogo.js";
 
 function AnimatedSplash(props) {
@@ -31,7 +31,7 @@ class Splash extends Component {
 
   render() {
     return this.state.redirect ? (
-      <Redirect to="/home" />
+      <Navigate to="/home" />
     ) : (
       <AnimatedSplash theme={this.props.theme} />
     );
