159f6712a680dabd294f57db2a0c489e20ac4c35
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 20:28:59 +0700

    Add view more button to project section on homepage

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,4 +1,6 @@
 import { projectsData } from '@/utils/data/projects-data';
+import { FaArrowRight } from 'react-icons/fa';
+import Link from 'next/link';
 import ProjectCard from './project-card';
 
 const Projects = () => {
@@ -29,6 +31,16 @@ const Projects = () => {
             </div>
           ))}
         </div>
+      </div>
+       <div className="flex justify-center  mt-5 lg:mt-12">
+        <Link
+          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+          role="button"
+          href="/projects"
+        >
+          <span>View More</span>
+          <FaArrowRight size={16} />
+        </Link>
       </div>
     </div>
   );
