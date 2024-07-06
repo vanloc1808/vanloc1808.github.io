51cb9509b73df2befff75b276c82e0d9285b1d43
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 20:25:41 +0700

    Update ProjectCard component

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -61,6 +61,15 @@ function ProjectCard({ project }) {
             <span className="text-cyan-400">{' ' + project.description}</span>
             <span className="text-gray-400">,</span>
           </div>
+        {/* Check if we have code field */}
+        {
+          project.code &&
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className="text-white">Code: </span>
+            <span className="text-cyan-400"><a href={project.code} target="_blank">{project.code}</a></span>
+            <span className="text-gray-400">,</span>
+          </div>
+        }
          {/* Check if we have backend_code field */}
           {
             project.backend_code &&
