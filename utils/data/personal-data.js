4b47b1e5be81b3edfda0e3ffa8452c33fee91bb9
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 11:15:43 +0700

    Add new template from Abu Said

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "VAN-LOC NGUYEN",
+  profile: "/profile_image.png",
+  designation: "Software Developer",
+  description: "My name is Van-Loc Nguyen. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "vanloc1808@gmail.com",
+  phone: "",
+  address: "District 5, Ho Chi Minh City, Vietnam",
+  github: "https://github.com/said7388",
+  facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/abu-said-bd/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "/CV.pdf"
+}
