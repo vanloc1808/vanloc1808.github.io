96d6af1fcfe4b0e90949f9b0473b271796619c95
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 20:26:01 +0700

    Update navbar navigation

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -33,7 +33,7 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOGS</div></Link>
           </li> */}
           <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
           </li>
         </ul>
       </div>
