754716d99c1f5e9a24dafdfd3dde0efe401700df
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 20:25:53 +0700

    Update project data

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,62 +1,69 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import ayla from "/public/image/ayla.jpg";
+import crefin from "/public/image/crefin.jpg";
+import realEstate from "/public/image/real-estate.jpg";
+import travel from "/public/image/travel.jpg";
 
 export const projectsData = [
-    // {
-    //     id: 1,
-    //     name: 'AI Powered Financial App',
-    //     description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-    //     tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-    //     role: 'Backend Developer',
-    //     code: '',
-    //     demo: '',
-    //     image: crefin,
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Travel Agency App',
-    //     description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-    //     tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-    //     role: 'Full Stack Developer',
-    //     code: '',
-    //     demo: '',
-    //     image: travel,
-    // },
-    // {
-    //     id: 3,
-    //     name: 'AI Powered Real Estate',
-    //     description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-    //     tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-    //     code: '',
-    //     role: 'Full Stack Developer',
-    //     demo: '',
-    //     image: realEstate,
-    // },
-    // {
-    //     id: 4,
-    //     name: 'Newsroom Management',
-    //     description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-    //     tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-    //     code: '',
-    //     demo: '',
-    //     image: ayla,
-    //     role: 'Full Stack Developer',
-    // }
-];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
+    {
+        id: 1,
+        name: "Connect",
+        description: "A powerful chat-app with features like Telegram. This is the project for Software Architecture course.",
+        tools: ["ReactJS", "Django REST Framework", "PostgreSQL", "Supabase", "Docker", "Jenkins", "GitHub Actions"],
+        role: "Backend Developer",
+        backend_code: "https://github.com/software-architecture-20tn/chat-app-backend",
+        frontend_code: "https://github.com/software-architecture-20tn/chat-app-frontend",
+        demo: "",
+        image: crefin,
+    },
+    {
+        id: 2,
+        name: "MiniShopping",
+        description: "A mobile application for e-commerce, allow users to buy online products. This is the project for Mobile Development course.",
+        tools: ["Android", "Django REST Framework", "NoSQL"],
+        role: "Android Developer",
+        backend_code: "https://github.com/hungt1/MiniShoppingServer",
+        frontend_code: "https://github.com/mekanican/MiniShopping",
+        demo: "",
+        image: crefin,
+    },
+    {
+        id: 3,
+        name: "PC Control via Email",
+        description: "A program that allows users to remotely control their computers using their emails. This is the project for Computer Network course.",
+        tools: ["Python", "Socket"],
+        role: "Socket Programmer",
+        code: "https://github.com/vanloc1808/computer-network-projects/tree/main/Socket-Email",
+        demo: "",
+        image: crefin,
+    },
+    {
+        id: 4,
+        name: "Chess Game",
+        description: "An offline game that allows PvP chess between two people. This is the project for Object-Oriented Programming course.",
+        tools: ["C++"],
+        role: "Main Developer",
+        code: "https://github.com/vanloc1808/chess-game",
+        demo: "",
+        image: crefin,
+    },
+    {
+        id: 5,
+        name: "Search Engine",
+        description: "An application that allows users to search for a string on a given dataset of text files. This is the project for Arts of Programming course.",
+        tools: ["C++"],
+        role: "Developer",
+        code: "https://github.com/vanloc1808/search-engine",
+        demo: "",
+        image: crefin,
+    },
+    {
+        id: 6,
+        name: "Search Engine",
+        description: "An application that performs arithmetic operations, logic operations on Big Integers. This is the project for Arts of Programming course.",
+        tools: ["C++"],
+        role: "Developer",
+        code: "https://github.com/mekanican/BigInteger",
+        demo: "",
+        image: crefin,
+    },
+]
