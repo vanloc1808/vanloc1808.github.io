7656772ebece6f37191ec18c067a6a2b0034d75e
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-06 20:46:26 +0700

    Remove legacy code

--- legacy/src/components/footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Footer.css";
-/* eslint-disable jsx-a11y/accessible-emoji */
-
-export default function Footer(props) {
-  return <div className="footer-div"></div>;
-}
