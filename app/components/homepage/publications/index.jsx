67fa572ce3be33ffa6e0d88222955707667ae424
Nguyen Van Loc <vanloc1808@gmail.com>
2024-07-07 19:51:57 +0700

    Add View More to publications

--- app/components/homepage/publications/index.jsx
+++ app/components/homepage/publications/index.jsx
@@ -4,6 +4,8 @@ import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 import experience from '/public/lottie/code.json';
+import { FaArrowRight } from 'react-icons/fa';
+import Link from 'next/link';
 
 function Publication() {
   return (
@@ -80,6 +82,16 @@ function Publication() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="flex justify-center  mt-5 lg:mt-12">
+        <Link
+          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+          role="button"
+          href="/publications"
+        >
+          <span>View More</span>
+          <FaArrowRight size={16} />
+        </Link>
       </div>
     </div>
   );
