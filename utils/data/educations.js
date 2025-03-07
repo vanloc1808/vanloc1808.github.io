2842c08c5258a171f9a274f2cf75a9daa5420987
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-03-07 16:25:48 +0700

    Update education information

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,8 +1,14 @@
 export const educations = [
   {
     id: 1,
+    title: "Master Degree in Artificial Intelligence",
+    duration: "Dec 2024 - Present",
+    institution: "University of Science, Vietnam National University Ho Chi Minh City"
+  },
+  {
+    id: 2,
     title: "Bachelor Degree in Computer Science, Honors Program",
-    duration: "2020 - Present",
-    institution: "University of Science, Vietnam National University Ho Chi Minh City",
+    duration: "Oct 2020 - Oct 2024",
+    institution: "University of Science, Vietnam National University Ho Chi Minh City"
   }
 ]
