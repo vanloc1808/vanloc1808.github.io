e3c377ef8f008402bca7ebcb56c76f7c844da4a9
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-03-16 22:35:10 +0700

    Update experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,11 +1,18 @@
 export const experiences = [
     {
+      id: 3,
+      title: "AI Engineer",
+      company: "Autonomous",
+      duration: "(Mar 2025 - Present)",
+      description: "Working on some AI projects at Glinks."
+    },
+    {
       id: 1,
       title: "Backend Developer (Python + Django)",
       company: "Saritasa - Builders of Better",
-      "companyLink": "https://www.saritasa.com/",
-      duration: "(Jun 2023 - Present)",
-      "description": "Back-end development with Django, and mainly with Django REST Framework. Build APIs that meet the specification of OpenAPI, implement unit tests, and write documentation for the APIs. Familiar with Postman and Swagger."
+      companyLink: "https://www.saritasa.com/",
+      duration: "(Jun 2023 - Feb 2025)",
+      description: "Back-end development with Django, and mainly with Django REST Framework. Build APIs that meet the specification of OpenAPI, implement unit tests, and write documentation for the APIs. Familiar with Postman and Swagger."
     },
     {
       id: 2,
@@ -13,6 +20,6 @@ export const experiences = [
       company: "Saritasa - Builders of Better",
       "companyLink": "https://www.saritasa.com/",
       duration: "(Mar 2023 - May 2023)",
-      "description": "Be trained with core Python modules and Django development, moreover with GitHub flow, Jira, Postman, and other skills. Build a project with Django and Django REST Framework."
+      description: "Be trained with core Python modules and Django development, moreover with GitHub flow, Jira, Postman, and other skills. Build a project with Django and Django REST Framework."
     }
 ]
