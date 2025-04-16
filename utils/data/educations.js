563f5888e65275923407d4a7ab792240086695dd
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-04-16 15:30:54 +0700

    Fix navbar navigation in News page

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,13 +1,13 @@
 export const educations = [
   {
     id: 1,
-    title: "Master Degree in Artificial Intelligence",
+    title: "Master of Science in Computer Science, majoring in Artificial Intelligence",
     duration: "Dec 2024 - Present",
     institution: "University of Science, Vietnam National University Ho Chi Minh City"
   },
   {
     id: 2,
-    title: "Bachelor Degree in Computer Science, Honors Program",
+    title: "Bachelor of Science in Computer Science, Honors Program",
     duration: "Oct 2020 - Oct 2024",
     institution: "University of Science, Vietnam National University Ho Chi Minh City"
   }
