9958d52096150f35fc0aa6e0db4c65933cc181c9
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-04-17 23:49:44 +0700

    Add light theme support

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -15,10 +15,10 @@ function ContactSection() {
   return (
     <div id="contact" className="my-12 lg:my-16 relative mt-24 text-white">
       <div className="hidden lg:flex flex-col items-center absolute top-24 -right-8">
-        <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
+        <span className="bg-gray-200 dark:bg-[#1a1443] text-gray-800 dark:text-white rotate-90 w-fit p-2 px-5 text-xl rounded-md">
           CONTACT
         </span>
-        <span className="h-36 w-[2px] bg-[#1a1443]"></span>
+        <span className="h-36 w-[2px] bg-gray-300 dark:bg-white"></span>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
         {
@@ -27,33 +27,34 @@ function ContactSection() {
         }
 
         <div className="lg:w-3/4 ">
-          <div className="flex flex-col gap-5 lg:gap-9">
-            <p className="text-sm md:text-xl flex items-center gap-3">
-              <MdAlternateEmail
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>{personalData.email}</span>
-            </p>
-            {/* <p className="text-sm md:text-xl flex items-center gap-3">
-              <IoMdCall
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>
-                {personalData.phone}
-              </span>
-            </p> */}
-            <p className="text-sm md:text-xl flex items-center gap-3">
-              <CiLocationOn
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>
-                {personalData.address}
-              </span>
-            </p>
-          </div>
+        <div className="flex flex-col gap-5 lg:gap-9">
+          <p className="text-sm md:text-xl flex items-center gap-3 text-gray-800 dark:text-white">
+            <MdAlternateEmail
+              className="bg-gray-300 dark:bg-[#1f223c] p-2 rounded-full text-gray-800 dark:text-white hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 cursor-pointer"
+              size={36}
+            />
+            <span>{personalData.email}</span>
+          </p>
+
+          {/* Uncomment this if you want to show phone again */}
+          {/*
+          <p className="text-sm md:text-xl flex items-center gap-3 text-gray-800 dark:text-white">
+            <IoMdCall
+              className="bg-gray-300 dark:bg-[#1f223c] p-2 rounded-full text-gray-800 dark:text-white hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 cursor-pointer"
+              size={36}
+            />
+            <span>{personalData.phone}</span>
+          </p>
+          */}
+
+          <p className="text-sm md:text-xl flex items-center gap-3 text-gray-800 dark:text-white">
+            <CiLocationOn
+              className="bg-gray-300 dark:bg-[#1f223c] p-2 rounded-full text-gray-800 dark:text-white hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 cursor-pointer"
+              size={36}
+            />
+            <span>{personalData.address}</span>
+          </p>
+        </div>
           <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
             <Link target="_blank" href={personalData.github}>
               <IoLogoGithub
