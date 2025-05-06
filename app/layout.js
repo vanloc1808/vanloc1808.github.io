58a6fa43f2a267c5bde6db2e7acd08896e0b2bfc
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-05-06 14:28:01 +0700

    Change metadata description

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Van-Loc Nguyen",
   description:
-    "This is the portfolio of Van-Loc Nguyen. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Van-Loc Nguyen. I am an AI researcher and engineer . I love to learn new things and I am always open to collaborating with others.",
 };
 
 export default function RootLayout({ children }) {
