9eb085a0191e8b4f38618d312ec39b8955704c9e
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-06-04 16:53:44 +0700

    Complete build fix: migrate all config files to ES modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -34,3 +34,5 @@ module.exports = {
   },
   plugins: [],
 };
+
+export default config;
