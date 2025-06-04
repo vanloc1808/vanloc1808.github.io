a0415a5622e9aff8605227256b0244c3da9f4a3e
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-06-04 13:15:01 +0700

    Add FastAPI to skills

--- utils/data/skills.ts
+++ utils/data/skills.ts
@@ -9,6 +9,7 @@ export const skillsData: string[] = [
   "Docker",
   "PostgreSQL",
   "Unity",
+  "FastAPI",
 ];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
