5a813af67873206247af29bd85ad1f1407ccf1ca
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-06-16 11:17:30 +0700

    Change single quote to double quote

--- utils/data/educations.ts
+++ utils/data/educations.ts
@@ -11,20 +11,20 @@ export const educations: Education[] = [
   {
     id: 1,
     title:
-      'Master of Science in Computer Science, majoring in Artificial Intelligence',
-    duration: 'Dec 2024 - Present',
+      "Master of Science in Computer Science, majoring in Artificial Intelligence",
+    duration: "Dec 2024 - Present",
     institution:
-      'University of Science, Vietnam National University Ho Chi Minh City',
-    logo: '/images/education/vnuhcm-logo.png',
-    secondLogo: '/images/education/hcmus-logo.png',
+      "University of Science, Vietnam National University Ho Chi Minh City",
+    logo: "/images/education/vnuhcm-logo.png",
+    secondLogo: "/images/education/hcmus-logo.png",
   },
   {
     id: 2,
-    title: 'Bachelor of Science in Computer Science, Honors Program',
-    duration: 'Oct 2020 - Oct 2024',
+    title: "Bachelor of Science in Computer Science, Honors Program",
+    duration: "Oct 2020 - Oct 2024",
     institution:
-      'University of Science, Vietnam National University Ho Chi Minh City',
-    logo: '/images/education/vnuhcm-logo.png',
-    secondLogo: '/images/education/hcmus-logo.png',
+      "University of Science, Vietnam National University Ho Chi Minh City",
+    logo: "/images/education/vnuhcm-logo.png",
+    secondLogo: "/images/education/hcmus-logo.png",
   },
 ];
