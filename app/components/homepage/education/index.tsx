8e3d05a06d9fd678d6ba4f1a5ae4652cb2856bef
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-07-01 17:25:21 +0700

    Update display on mobile UI

--- app/components/homepage/education/index.tsx
+++ app/components/homepage/education/index.tsx
@@ -96,9 +96,9 @@ const Education: FC = () => {
                           </div>
                         </div>
 
-                        <div className='flex items-start gap-x-3 px-6 py-4 sm:gap-x-4 lg:gap-x-6'>
+                        <div className='flex items-start gap-x-2 px-6 py-4 sm:gap-x-4 lg:gap-x-6'>
                           {education.secondLogo ? (
-                            <div className='flex flex-shrink-0 flex-col items-center gap-1 sm:flex-row sm:gap-x-2 lg:gap-x-3'>
+                            <div className='flex flex-shrink-0 flex-col items-start gap-1 sm:flex-row sm:items-center sm:gap-x-2 lg:gap-x-3'>
                               <div className='relative h-10 w-10 overflow-hidden rounded-full shadow-lg dark:drop-shadow-[0_0_10px_white] sm:h-12 sm:w-12 lg:h-16 lg:w-16'>
                                 <Image
                                   src={education.logo}
