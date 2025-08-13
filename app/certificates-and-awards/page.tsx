715d779106af91f3f8a217a3277d4edca22a840a
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-08-13 15:09:13 +0700

    Change Certificates & Awards icon to education-themed animation

--- app/certificates-and-awards/page.tsx
+++ app/certificates-and-awards/page.tsx
@@ -2,7 +2,7 @@
 
 import { FC } from 'react';
 import { getCertificates } from '@/utils/data/certificates-data';
-import experiences from '/public/lottie/code.json';
+import certificateLottie from '/public/lottie/education.json';
 import dynamic from 'next/dynamic';
 import { useTranslation } from '../context/I18nContext';
 import { FaTrophy, FaCertificate } from 'react-icons/fa';
@@ -32,7 +32,7 @@ const CertificatesAndAwards: FC = () => {
         <div className='grid grid-cols-1 gap-6 lg:grid-cols-3 lg:gap-8'>
           <div className='flex items-start justify-center lg:pr-4'>
             <div className='h-full w-full max-w-md'>
-              <AnimationLottie animationData={experiences} />
+              <AnimationLottie animationData={certificateLottie} />
             </div>
           </div>
 
