f7311a9f4f07839d4fcf1e1c2c863c517018b415
Van-Loc Nguyen <vanloc1808@gmail.com>
2025-11-10 14:09:10 +0900

    Update address

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -42,7 +42,8 @@ export const personalData: MultilingualPersonalData = {
       '👋 My name is Van-Loc Nguyen.\n\n💻 I am a professional and enthusiastic programmer in my daily life.\n\n🚀 I am a quick learner with a self-learning attitude.\n\n🔍 I love to learn and explore new technologies and am passionate about problem-solving.\n\n🤖 I love Artificial Intelligence, especially Computer Vision and Deep Learning.\n\n🐍 My core skill is based on Python and I love almost all of the things with Python.',
     email: 'contact@nguyenvanloc.com',
     phone: '',
-    address: 'Chợ Quán Ward, Hồ Chí Minh City, Vietnam',
+    // address: 'Ho Chi Minh City, Vietnam',
+    address: 'Tokyo, Japan',
     github: 'https://github.com/vanloc1808',
     googleScholar:
       'https://scholar.google.com/citations?user=39z1A1IAAAAJ&hl=en',
@@ -68,7 +69,8 @@ export const personalData: MultilingualPersonalData = {
       '👋 Xin chào, tôi là Nguyễn Văn Lộc.\n\n💻 Tôi là một lập trình viên chuyên nghiệp, luôn tràn đầy nhiệt huyết trong công việc hằng ngày.\n\n🚀 Tôi luôn giữ tinh thần tự học cao.\n\n🔍 Tôi đam mê tìm hiểu, khám phá công nghệ mới và yêu thích việc giải quyết các bài toán thực tiễn.\n\n🤖 Tôi đặc biệt yêu thích lĩnh vực Trí tuệ Nhân tạo, nhất là Thị giác Máy tính và Học sâu.\n\n🐍 Kỹ năng chính của tôi là Python, và tôi yêu thích gần như mọi thứ liên quan đến ngôn ngữ này.',
     email: 'contact@nguyenvanloc.com',
     phone: '',
-    address: 'Phường Chợ Quán, Thành phố Hồ Chí Minh, Việt Nam',
+    // address: 'Thành phố Hồ Chí Minh, Việt Nam',
+    address: 'Tokyo, Nhật Bản',
     github: 'https://github.com/vanloc1808',
     googleScholar:
       'https://scholar.google.com/citations?user=39z1A1IAAAAJ&hl=en',
